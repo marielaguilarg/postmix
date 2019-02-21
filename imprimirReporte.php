6f638bb824816a43bd926d4856d998d923e1af02
Marisol <Marisol@PC>
2019-02-21 17:13:57 -0600

    juntando versiones

--- imprimirReporte.php
+++ imprimirReporte.php
@@ -1,15 +1,8 @@
 <?php
 session_start();
 if (isset($_SESSION['Usuario'])) { //valido que este logeado
- 
-
 
 error_reporting(E_ERROR|E_PARSE);
-
-//error_reporting(E_ALL);
- 
-
- 
   require "Models/crud_abierta.php";
   require "Models/crud_ponderacion.php";
   require "Models/crud_imagendetalle.php";
@@ -77,10 +70,29 @@ $imp->descargarArchivo();
 {
 	include 'Controllers/recepcionController.php';
 	include "Utilerias/utilerias.php";
-	include "Models/crud_catalogodetalle.php";
+	include "Models/crud_catalogoDetalle.php";
 	include "Models/crud_muestras.php";
 	$recController=new RecepcionController();
 	$recController->imprimir();
+	
+}else if($admin=="impetiq")
+{
+	include 'Controllers/muestrasController.php';
+	include "Models/crud_catalogoDetalle.php";
+	include "Models/crud_muestras.php";
+	include "Utilerias/utilerias.php";
+	$muestraController=new muestrasController;
+	$muestraController->imprimir();
+	
+}else if($admin=="Tarcr")
+{
+	
+	include "Utilerias/utilerias.php";
+	include "Models/crud_catalogoDetalle.php";
+	include "Models/crud_muestras.php";
+	include "Controllers/archivoAguaController.php";
+	$aguaController=new ArchivoAguaController();
+	$aguaController->generarArchivo();
 	
 }else
     { 
@@ -94,8 +106,5 @@ $imp->descargarArchivo();
     }
     
 } else {
-    include "Controllers/controller.php";
-    $mvc =new MvcController();
-  
-    $mvc -> inicio();
+    header("location:index.php");
 }
